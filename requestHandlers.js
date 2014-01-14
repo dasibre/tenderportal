7eed6df314a7b25cd61658f9c9202e511ce72788
James <james.naadjie@gmail.com>
2014-01-13 19:35:03 -0500

    req handler start

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,13 +4,15 @@ var exec = require("child_process").exec,
 
 function start(response) {
 	console.log("Request handler 'start' was called. ");
-	var content = "empty";
-
-	exec("ls -lah", function(error, stdout, stderr){
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
+		response.write("Tender Portal");
 		response.end();
-	});
+
+	// exec("ls -lah", function(error, stdout, stderr){
+	// 	response.writeHead(200, {"Content-Type": "text/plain"});
+	// 	response.write(stdout);
+	// 	response.end();
+	// });
 }
 
 function upload(response) {
